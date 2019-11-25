20adf702af7cf62a0d324ab51a931e14a361e395
Sravan Ankam <Sravan_Ankam@epam.com>
2019-11-25 10:24:35 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: 'https://makemy360api.azurewebsites.net/Api/',
-  tokenUrl:'https://makemy360api.azurewebsites.net/',
-  webApp:"https://makemy360app.azurewebsites.net",
+  apiUrl: 'https://makemy360apiservice.azurewebsites.net/Api/',
+  tokenUrl:'https://makemy360apiservice.azurewebsites.net/',
+  webApp:"https://makemy360appservices.azurewebsites.net",
   production: true
 };
